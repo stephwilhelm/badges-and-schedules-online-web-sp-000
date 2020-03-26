0f090c8fb0d4a82abed8cc46e16987f72f218ac0
Steph W <wilhelm.steph@gmail.com>
2020-03-26 23:05:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,11 @@
+attendees = ["Edsger, Ada, Charles, Alan, Grace, Linus, Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (name)
-  attendees = ["Edsger, Ada, Charles, Alan, Grace, Linus, Matz"]
-  attendees.each do
-    puts "Hello, my name is #{name}."
+def batch_badge_creator (attendees)
+
+  attendees.each do |attendee|
+    puts "Hello, my name is #{attendee}."
 end
