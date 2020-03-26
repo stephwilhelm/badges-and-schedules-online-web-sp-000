f69e2bfb9c8b7e1d4e5eabb4aeb04ba5d9b4786f
Steph W <wilhelm.steph@gmail.com>
2020-03-26 23:27:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(attendees)
     "Hello, my name is #{attendee}."
   end
 end
+
+def assign_rooms (attendees)
+  rooms = ["1-7"]
+
+  attendees.each do |attendee|
+    prints "Hello, #{attendee}! You'll be assigned to room #{rooms}!"
+  end
+end
